055275df2e28cf0de09bdec61465cc43d8148f2d
AayushBaniya2006 <111075880+AayushBaniya2006@users.noreply.github.com>
2023-04-04 21:40:30 -0500

    Error Management

--- API.py
+++ API.py
@@ -1,5 +1,5 @@
 import requests
-requested_school = input("What school would you like to go too?")
+requested_school = input("What school would you like to go too? ")
 
 # Set the API endpoint URL
 url = 'https://api.data.gov/ed/collegescorecard/v1/schools'
@@ -16,23 +16,21 @@ params = {
 }
 
 # Make the API call
-response = requests.get(url, params=params)
-
-# Get the admission rate, SAT score midpoint, and ACT score midpoint data from the response
-result = response.json()['results'][0]
-admission_rate = result['latest.admissions.admission_rate.overall']
-sat_math_midpoint = result['latest.admissions.sat_scores.midpoint.math']
-sat_reading_midpoint = result['latest.admissions.sat_scores.midpoint.critical_reading']
-sat_writing_midpoint = result['latest.admissions.sat_scores.midpoint.writing']
-act_cumulative_midpoint = result['latest.admissions.act_scores.midpoint.cumulative']
-# Print the admission rate, SAT score midpoint, and ACT score midpoint data
-print(f"The admission rate for {params['school.name']} is {admission_rate}")
-print(f"The SAT score midpoint for Math is {sat_math_midpoint}")
-print(f"The SAT score midpoint for Critical Reading is {sat_reading_midpoint}")
-print(f"The SAT score midpoint for Writing is {sat_writing_midpoint}")
-print(f"The ACT score midpoint (cumulative) is {act_cumulative_midpoint}")
-
-
-
-
-
+try:
+    response = requests.get(url, params=params)
+    # Get the admission rate, SAT score midpoint, and ACT score midpoint data from the response
+    result = response.json()['results'][0]
+    admission_rate = result['latest.admissions.admission_rate.overall']
+    sat_math_midpoint = result['latest.admissions.sat_scores.midpoint.math']
+    sat_reading_midpoint = result['latest.admissions.sat_scores.midpoint.critical_reading']
+    sat_writing_midpoint = result['latest.admissions.sat_scores.midpoint.writing']
+    act_cumulative_midpoint = result['latest.admissions.act_scores.midpoint.cumulative']
+    # Print the admission rate, SAT score midpoint, and ACT score midpoint data
+    print(f"The admission rate for {params['school.name']} is {admission_rate}")
+    print(f"The SAT score midpoint for Math is {sat_math_midpoint}")
+    print(f"The SAT score midpoint for Critical Reading is {sat_reading_midpoint}")
+    print(f"The SAT score midpoint for Writing is {sat_writing_midpoint}")
+    print(f"The ACT score midpoint (cumulative) is {act_cumulative_midpoint}")
+except:
+    print(" \n ERROR, please only put 1 school at a time, and be sure you are spelling the University correctly. \n")
+    
