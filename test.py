ed5e9c5cba5b7ddc37cac7ef6be832326d7d6634
Psangs123 <39105705+FearlessTiger@users.noreply.github.com>
2023-09-27 14:48:17 -0500

    got email phrasing to work

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import nltk
 import re
 
-nltk.download()
-
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tag import pos_tag
 
@@ -15,39 +13,21 @@ def parse_resume(resume_text):
     experience = []
     education = []
 
-    # Tokenize the resume text into sentences
-    sentences = sent_tokenize(resume_text)
+    text = re.sub(r"[^a-zA-Z0-9]", " ", resume_text.lower())
+    words = text.split()
 
-    for sentence in sentences:
-        # Tokenize each sentence into words
-        words = word_tokenize(sentence)
+    sentances = sent_tokenize(resume_text)
+    email_expression = "\A[\w!#$%&'*+/=?`{|}~^-]+(?:\.[\w!#$%&'*+/=?`{|}~^-]+)*@↵(?:[A-Z0-9-]+\.)+[A-Z]{2,6}\Z"
+    emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", resume_text)
+    return emails
+    
 
-        # Perform part-of-speech tagging to identify named entities
-        tagged_words = pos_tag(words)
 
-        # Extract information based on named entities and patterns
-        for word, tag in tagged_words:
-            if tag == 'NNP' and not name:
-                name = word
-            elif re.match(r'\S+@\S+', word) and not email:
-                email = word
-            elif re.match(r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}', word) and not phone:
-                phone = word
 
-        # You can add more parsing logic here to extract skills, experience, and education.
-
-    return {
-        "Name": name,
-        "Email": email,
-        "Phone": phone,
-        "Skills": skills,
-        "Experience": experience,
-        "Education": education
-    }
-
-if __name__ == "__main__":
-    # Example resume text (replace with your actual resume text)
-    resume_text = """
+    return sentences
+    
+    
+resume_text = """
     John Doe
     john.doe@email.com
     (123) 456-7890
@@ -64,9 +44,4 @@ if __name__ == "__main__":
     Education:
     - Bachelor's in Computer Science, University Y, 2019
     """
-
-    parsed_resume = parse_resume(resume_text)
-
-    # Print the parsed information
-    for key, value in parsed_resume.items():
-        print(f"{key}: {value}")
+print(parse_resume(resume_text))
